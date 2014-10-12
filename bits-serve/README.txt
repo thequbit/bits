d85da533c77b341ce047c4ff475d85497de82622
thequbit <thequbit@gmail.com>
2014-10-12 10:35:11 -0400

    adding pyramid app

---
+++ bits-serve/README.txt
@@ -0,0 +1,14 @@
+bits-serve README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/python setup.py develop
+
+- $VENV/bin/initialize_bits-serve_db development.ini
+
+- $VENV/bin/pserve development.ini
+
---
+++ bits-serve/setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='bits-serve',
+      version='0.0',
+      description='bits-serve',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='bitsserve',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = bitsserve:main
+      [console_scripts]
+      initialize_bits-serve_db = bitsserve.scripts.initializedb:main
+      """,
+      )
