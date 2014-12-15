71ccc0327c88821743caf63b0a3c1c98b8ec8d51
thequbit <thequbit@gmail.com>
2014-12-14 19:28:39 -0500

    default config file

---
+++ bits-serve/bitsserve/config.py
@@ -0,0 +1,10 @@
+config = {
+
+    'root_domain': '',
+
+    'notification_email_server': '',
+    'notification_email_server_port': 0000,
+    'notification_email_address': '',
+    'notification_email_password': '',
+    
+}
